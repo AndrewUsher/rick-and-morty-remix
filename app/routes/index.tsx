8b863c4b6d7c0c39ad3bc0523238338026a07702
Andrew Usher <andrewusher00@gmail.com>
2021-12-05 19:12:23 -0600

    chore: run linting

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,8 @@
 export default function IndexRoute () {
-  return ( 
+  return (
     <main className="h-screen bg-gradient-to-b from-green-600 via-green-500 to-green-800">
       <h2 className="pt-16 px-8 text-center text-3xl font-bold">
-        Welcome to Mortypedia, the home of everything Rick and Morty! 
+        Welcome to Mortypedia, the home of everything Rick and Morty!
         Feel free to look around at the characters, locations, and episodes.
       </h2>
     </main>
