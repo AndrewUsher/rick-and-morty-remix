19c62b14384a4e425fab4ccf74972107aa83f823
Andrew Usher <andrewusher00@gmail.com>
2021-12-06 21:59:45 -0600

    fix: add responsive viewport meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ export default function App () {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
