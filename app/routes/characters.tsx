55c4c9596424f3dfb8a88d5dea1f1ee6efee63e6
gdavis <gregory.davis@autozone.com>
2022-08-31 16:52:01 -0400

    feat(scroll): option to autoscroll on /characters route
    - TODO:
     - throttle the scroll event for added performance
     - automatically switch icon to up when the page is fully at the bottom of the screen
     - automatically switch the icon to down when the page is fully at the top of the screen
     - hide the icon when user is not scrolling

--- app/routes/characters.tsx
+++ app/routes/characters.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode } from 'react'
+import React, { useEffect, useState, type ReactNode } from 'react'
 import { LoaderFunction, MetaFunction, useFetcher, useLoaderData } from 'remix'
 
 enum Gender {
@@ -19,20 +19,22 @@ enum Status {
 }
 
 type Character = {
-  gender: Gender,
-  id: number,
-  image: string,
-  location: Location,
-  name: string,
-  origin: Location,
-  species: string,
-  status: Status
-}
+  gender: Gender;
+  id: number;
+  image: string;
+  location: Location;
+  name: string;
+  origin: Location;
+  species: string;
+  status: Status;
+};
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const url = new URL(request.url)
   const pageNumber = url.searchParams.get('page') || 1
-  const res = await fetch(`https://rickandmortyapi.com/api/character?page=${pageNumber}`)
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${pageNumber}`
+  )
   const data = await res.json()
 
   const parsedResults = data.results.map((character: Character) => ({
@@ -57,13 +59,15 @@ export const meta: MetaFunction = () => {
 }
 
 type LineItemProps = {
-  label: string,
-  value: string
-}
+  label: string;
+  value: string;
+};
 
 function LineItem ({ label, value }: LineItemProps) {
   return (
-    <p className="flex justify-between items-baseline border-b-2 mb-2 pb-2 text-lg font-semibold">{label} <span className="font-normal text-base">{value}</span></p>
+    <p className="flex justify-between items-baseline border-b-2 mb-2 pb-2 text-lg font-semibold">
+      {label} <span className="font-normal text-base">{value}</span>
+    </p>
   )
 }
 
@@ -71,20 +75,68 @@ let pageNumber = 1
 
 export default function CharactersRoute (): ReactNode {
   const data = useLoaderData<Character[]>()
-  const [results, setResults] = React.useState(data)
+
+  const [results, setResults] = useState(data)
+  const [scrollingDown, setScrollingDown] = useState(true)
+
   const fetcher = useFetcher()
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (fetcher.data) {
-      setResults((prevResults: Character[]) => [...prevResults, ...fetcher.data])
+      setResults((prevResults: Character[]) => [
+        ...prevResults,
+        ...fetcher.data
+      ])
     }
   }, [fetcher.data])
+
+  useEffect(() => {
+    let scrollPosition = 0
+    let scrollDirection
+
+    window.addEventListener('scroll', function (e) {
+      scrollDirection = (document.body.getBoundingClientRect()).top > scrollPosition ? 'up' : 'down'
+      scrollPosition = (document.body.getBoundingClientRect()).top
+      setScrollingDown(scrollDirection === 'down')
+    })
+    return () => {
+      window.removeEventListener('scroll', function (e) {
+        scrollDirection = (document.body.getBoundingClientRect()).top > scrollPosition ? 'up' : 'down'
+        scrollPosition = (document.body.getBoundingClientRect()).top
+        setScrollingDown(scrollDirection === 'down')
+      })
+    }
+  }, [])
+
+  const scrollTo = (x: number, y: number) => {
+    window.scrollTo(x, y)
+  }
+  const scrollToTop = () => {
+    scrollTo(0, 0)
+  }
+  const scrollToBottom = () => {
+    scrollTo(0, document.body.scrollHeight)
+  }
+
+  const handleOnClick = () => {
+    if (scrollingDown) {
+      scrollToBottom()
+    } else {
+      scrollToTop()
+    }
+  }
+
   return (
     <main className="py-4 container mx-auto">
       <div className="grid gap-x-4 gap-y-8 grid-cols-1 lg:grid-cols-4 sm:grid-cols-1 mb-8">
         {results.map((character, index) => (
           <article key={character.id} className="mx-auto pb-4 shadow w-full">
-            <img className="w-full mb-4" loading={index < 2 ? 'eager' : 'lazy'} src={character.image} alt="" />
+            <img
+              className="w-full mb-4"
+              loading={index < 2 ? 'eager' : 'lazy'}
+              src={character.image}
+              alt=""
+            />
             <div className="pl-8 pr-8">
               <h2 className="mb-4 text-2xl font-bold">{character.name}</h2>
               <LineItem label="Gender" value={character.gender} />
@@ -94,6 +146,39 @@ export default function CharactersRoute (): ReactNode {
           </article>
         ))}
       </div>
+      <div className="fixed bottom-10 right-10 animate-pulse scale-125">
+        <button onClick={handleOnClick}>
+          {scrollingDown ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
       <button
         className="mx-auto my-4 block bg-green-700 text-white px-4 py-4 text-lg shadow-md disabled:bg-gray-400 disabled:text-black disabled:line-through"
         disabled={fetcher.state === 'submitting'}
@@ -101,7 +186,7 @@ export default function CharactersRoute (): ReactNode {
           pageNumber++
           fetcher.submit(new URLSearchParams(`page=${pageNumber}`))
         }}>
-          Next Page
+        Next Page
       </button>
     </main>
   )
